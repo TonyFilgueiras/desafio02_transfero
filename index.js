41143eb175eb284287d86b9c00f0789811b532fe
TonyFilgueiras <ant.mosquera@yahoo.com.br>
2022-05-09 09:38:50 -0300

    adding json files

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function graph(data, coin,time, chart = myChart) {
             dataset.data = data;
         }
     });
-    chart.options.scales.x.min = dados_ano_ibov.length - time
-    chart.options.scales.x.max = dados_ano_ibov.length - 1
+    chart.options.scales.x.min = label_ano.length - time
+    chart.options.scales.x.max = label_ano.length - 1
     chart.update();
 }
 
@@ -77,11 +77,7 @@ function language_change() {
 
 
 // ------------------------------------My testing data----------------------------------------------------------------//
-
-let dados_ano_ibov = [103922.0,103514.0,101006.0,101561.0,102719.0,101945.0,103779.0,105686.0,105530.0,106928.0,106692.0,106522.0,108013.0,109102.0,108942.0,107752.0,109845.0,111573.0,112315.0,111478.0,112388.0,113147.0,112161.0,111696.0,112245.0,111996.0,112234.0,112461.0,113359.0,113572.0,113807.0,114660.0,115181.0,113528.0,112768.0,111725.0,112892.0,112008.0,111592.0,113142.0,115174.0,115166.0,114474.0,111593.0,111203.0,113900.0,113663.0,111713.0,109928.0,108959.0,111112.0,113076.0,115311.0,116155.0,117272.0,117457.0,119053.0,119081.0,118738.0,120014.0,120260.0,119999.0,121570.0,121280.0,118885.0,118228.0,118862.0,118322.0,116953.0,116147.0,116782.0,116182.0,115687.0,115057.0,114344.0,111078.0,110685.0,108213.0,109349.0,109919.0,107876.0,106639.0]
 let label_ano = ['2022-01-03','2022-01-04','2022-01-05','2022-01-06','2022-01-07','2022-01-10','2022-01-11','2022-01-12','2022-01-13','2022-01-14','2022-01-17','2022-01-18','2022-01-19','2022-01-20','2022-01-21','2022-01-24','2022-01-25','2022-01-26','2022-01-27','2022-01-28','2022-01-31','2022-02-01','2022-02-02','2022-02-03','2022-02-04','2022-02-07','2022-02-08','2022-02-09','2022-02-10','2022-02-11','2022-02-14','2022-02-15','2022-02-16','2022-02-17','2022-02-18','2022-02-21','2022-02-22','2022-02-23','2022-02-24','2022-02-25','2022-03-02','2022-03-03','2022-03-04','2022-03-07','2022-03-08','2022-03-09','2022-03-10','2022-03-11','2022-03-14','2022-03-15','2022-03-16','2022-03-17','2022-03-18','2022-03-21','2022-03-22','2022-03-23','2022-03-24','2022-03-25','2022-03-28','2022-03-29','2022-03-30','2022-03-31','2022-04-01','2022-04-04','2022-04-05','2022-04-06','2022-04-07','2022-04-08','2022-04-11','2022-04-12','2022-04-13','2022-04-14','2022-04-18','2022-04-19','2022-04-20','2022-04-22','2022-04-25','2022-04-26','2022-04-27','2022-04-28','2022-04-29','2022-05-02']
-let coin = "IBOV"
-let dados_ano_ibov_percentage = []
 
         
 // ----------------------------------------Scroll no grafico-----------------------------------------------------//
@@ -97,9 +93,9 @@ function scroll_right(chart = myChart) {
     }
     
     
-    if(chart.options.scales.x.max >= dados_ano_ibov.length -1) {
-        chart.options.scales.x.max = dados_ano_ibov.length -1
-        chart.options.scales.x.min = dados_ano_ibov.length - time
+    if(chart.options.scales.x.max >= label_ano.length -1) {
+        chart.options.scales.x.max = label_ano.length -1
+        chart.options.scales.x.min = label_ano.length - time
         
     };
     
@@ -150,15 +146,14 @@ function checking_for_percentage(dados1, dados2) {
     } return x
 }
 
-
-percentage_calculator(dados_ano_ibov, dados_ano_ibov_percentage)
 percentage_calculator(btc_value, btc_value_percentage)
 
 // -----------------------------------Changing graph----------------------------------------------//
 
 function graph_change() {
-    var z = checking_for_percentage(dados_ano_ibov,dados_ano_ibov_percentage)
-    var y = checking_for_percentage(btc_value,btc_value_percentage)
+    var ibov = checking_for_percentage(ibovespa_value,ibovespa_value_percentage)
+    var btc = checking_for_percentage(btc_value,btc_value_percentage)
+    var dollar = checking_for_percentage(dollar_value,dollar_value_percentage)
     if (semanaradio.checked) {
         semanabox.style.background = "#56EEF4"
         mesbox.style.background = "none"
@@ -166,8 +161,9 @@ function graph_change() {
         document.querySelector("#semana>span").style.color = "black"
         mesbox.style.color = "white"
         ano.style.color = "white"
-        graph(z,"IBOV", 6)
-        graph(y, "BTC",6)
+        graph(ibov,"IBOV", 6)
+        graph(btc, "BTC",6)
+        graph(dollar, "Dollar",6)
         arrow_left.style.visibility = "visible"
         arrow_right.style.visibility = "visible"
     }
@@ -178,8 +174,9 @@ function graph_change() {
         semana.style.color = "white"
         mesbox.style.color = "black"
         ano.style.color = "white"
-        graph(z,'IBOV', 22)
-        graph(y, "BTC",22)
+        graph(ibov,'IBOV', 22)
+        graph(btc, "BTC",22)
+        graph(dollar, "Dollar",22)
         arrow_left.style.visibility = "visible"
         arrow_right.style.visibility = "visible"
     }
@@ -190,8 +187,9 @@ function graph_change() {
         document.querySelector("#semana>span").style.color = "white"
         mesbox.style.color = "white"
         ano.style.color = "black"
-        graph(z, "IBOV", dados_ano_ibov.length)
-        graph(y, "BTC", btc_value.length)
+        graph(ibov, "IBOV", ibovespa_value.length)
+        graph(btc, "BTC", btc_value.length)
+        graph(dollar, "Dollar",dollar_value.length)
         arrow_left.style.visibility = "hidden"
         arrow_right.style.visibility = "hidden"
     }    
@@ -220,6 +218,12 @@ function total_viewer() {
 var poupanca_value = []
 var btc_value = []
 var btc_value_percentage = []
+var dollar_value = []
+var dollar_value_percentage = []
+var cdi_value = []
+var ibovespa_value = []
+var ibovespa_value_percentage = []
+var selic_value = []
 
 // function read_json (file_name, list){
 //     fetch(file_name)
@@ -234,7 +238,7 @@ var btc_value_percentage = []
 // poupanca_value = await read_json('./poupanca.json', poupanca_value)
 // console.log(poupanca_value)
 
-await fetch('./poupanca.json')
+await fetch('./json_files/poupanca.json')
     .then(function(resp){
         return resp.json();
     })
@@ -242,21 +246,62 @@ await fetch('./poupanca.json')
         poupanca_value = data.map(x => x.valor)
     })
 
-await fetch('./btc.json')
+await fetch('./json_files/btc.json')
     .then(function(resp){
         return resp.json();
     })
     .then(function(data){
         btc_value = data.map(x => x.valor)
     })
-await fetch('./btc.json')
+await fetch('./json_files/btc.json')
     .then(function(resp){
         return resp.json();
     })
     .then(function(data){
         btc_value_percentage = data.map(x => x.variacao)
     })
-
+await fetch('./json_files/dollar.json')
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        dollar_value = data.map(x => x.valor)
+    })
+await fetch('./json_files/dollar.json')
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        dollar_value_percentage = data.map(x => x.variacao)
+    })
+await fetch('./json_files/cdi.json')
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        cdi_value = data.map(x => x.valor)
+    })
+await fetch('./json_files/ibovespa.json')
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        ibovespa_value = data.map(x => x.valor)
+    })
+await fetch('./json_files/ibovespa.json')
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        ibovespa_value_percentage = data.map(x => x.variacao)
+    })
+await fetch('./json_files/selic.json')
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        selic_value = data.map(x => x.valor)
+    })
 
 
 // ------------------------------------------Plotting graph-------------------------------------------------------//
@@ -265,7 +310,7 @@ const data = {
     labels: label_ano,
     datasets: [{
         label: "IBOV",
-        data: dados_ano_ibov,
+        data: ibovespa_value,
         backgroundColor: [
             '#357DED',   
         ],
@@ -295,6 +340,39 @@ const data = {
         borderColor: [
                     'orange'
                 ],
+        borderWidth: 0.8,
+    },
+    {
+        label: 'Dollar',
+        data: dollar_value,
+        backgroundColor: [
+            'red',
+        ],
+        borderColor: [
+            'red',
+                    ],
+        borderWidth: 0.8,
+    },
+    {
+        label: 'CDI',
+        data: cdi_value,
+        backgroundColor: [
+            'yellow',
+        ],
+        borderColor: [
+            'yellow',
+                    ],
+        borderWidth: 0.8,
+    },
+    {
+        label: 'Selic',
+        data: selic_value,
+        backgroundColor: [
+            'green',
+        ],
+        borderColor: [
+            'green',
+                    ],
         borderWidth: 0.8,
     },]
 };
