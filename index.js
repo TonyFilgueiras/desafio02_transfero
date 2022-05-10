8150b2547427a4b289b0c5753fa36eae206ee865
TonyFilgueiras <ant.mosquera@yahoo.com.br>
2022-05-10 12:58:01 -0300

    Adding a monkey

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ window.scroll_left = scroll_left;
 window.scroll_right = scroll_right;
 window.percentage_calculator = percentage_calculator
 window.checking_for_percentage = checking_for_percentage
+window.visible_value = visible_value
 
 
 
@@ -45,31 +46,66 @@ const arrow_left = document.querySelector("#setaesquerda")
 const arrow_right = document.querySelector("#setadireita")
 
 
-
-// let language_factor = 1 //important to change graph label according to language selection
-const teste = document.getElementById("teste")
+const total_carteira = document.getElementById("total_carteira_h1")
+const total_carteira_valor = document.getElementById("total_carteira_valor")
 
 
 const englishradio = document.getElementById("englishradio")
 const portuguesradio = document.getElementById("portuguesradio")
 
+const valor_investido = document.getElementsByClassName("valor_investido")
+const totalzao = document.getElementsByClassName("totalzao")
+
+const monkey = document.getElementById("monkey")
+
+
+let invested_value_ibov = 1000
+let invested_value_poupanca = 1000
+let invested_value_btc = 1000
+let invested_value_dollar = 1000
+let invested_value_cdi = 1000
+let invested_value_selic = 1000
+const bolsa_ibov = document.querySelector("#ibovespa label")
+const bolsa_ibov_total = document.querySelector("#ibovespa .valor_total")
+const bolsa_poupanca = document.querySelector("#poupanca label")
+const bolsa_poupanca_total = document.querySelector("#poupanca .valor_total")
+const bolsa_btc = document.querySelector("#btc label")
+const bolsa_btc_total = document.querySelector("#btc .valor_total")
+const bolsa_dollar = document.querySelector("#dollar label")
+const bolsa_dollar_total = document.querySelector("#dollar .valor_total")
+const bolsa_cdi = document.querySelector("#cdi label")
+const bolsa_cdi_total = document.querySelector("#cdi .valor_total")
+const bolsa_selic = document.querySelector("#selic label")
+const bolsa_selic_total = document.querySelector("#selic .valor_total")
+
 // ----------------------------------Changing Language-----------------------------------------------//
 
 function language_change() {
     if (englishradio.checked == true) {
-        teste.innerHTML="English man..."    
+        total_carteira.innerHTML="Total Value:"    
         semana.innerText = "Week"
         mes.innerText = "Month"
         ano.innerText = "Year"
-        language_factor = 0
+        for (let i = 0; i < valor_investido.length; i++) {
+            valor_investido[i].innerText = "Invested Value:";
+        }
+        for (let i = 0; i < valor_investido.length; i++) {
+            totalzao[i].innerText = "Asset Value:";
+        }
         graph_change()
     }
     else  {
-        teste.innerHTML="Português mano..."
+        total_carteira.innerHTML="Total na Carteira:"
         semana.innerText = "Semana"
         mes.innerText = "Mês"
         ano.innerText = "Ano"
-        language_factor = 1
+        valor_investido.innerText = "Valor Investido"
+        for (let i = 0; i < valor_investido.length; i++) {
+            valor_investido[i].innerText = "Valor Investido:";
+        }
+        for (let i = 0; i < valor_investido.length; i++) {
+            totalzao[i].innerText = "Valor Total:";
+        }
         graph_change()
     }
 }
@@ -77,7 +113,46 @@ function language_change() {
 
 
 // ------------------------------------My testing data----------------------------------------------------------------//
-let label_ano = ['2022-01-03','2022-01-04','2022-01-05','2022-01-06','2022-01-07','2022-01-10','2022-01-11','2022-01-12','2022-01-13','2022-01-14','2022-01-17','2022-01-18','2022-01-19','2022-01-20','2022-01-21','2022-01-24','2022-01-25','2022-01-26','2022-01-27','2022-01-28','2022-01-31','2022-02-01','2022-02-02','2022-02-03','2022-02-04','2022-02-07','2022-02-08','2022-02-09','2022-02-10','2022-02-11','2022-02-14','2022-02-15','2022-02-16','2022-02-17','2022-02-18','2022-02-21','2022-02-22','2022-02-23','2022-02-24','2022-02-25','2022-03-02','2022-03-03','2022-03-04','2022-03-07','2022-03-08','2022-03-09','2022-03-10','2022-03-11','2022-03-14','2022-03-15','2022-03-16','2022-03-17','2022-03-18','2022-03-21','2022-03-22','2022-03-23','2022-03-24','2022-03-25','2022-03-28','2022-03-29','2022-03-30','2022-03-31','2022-04-01','2022-04-04','2022-04-05','2022-04-06','2022-04-07','2022-04-08','2022-04-11','2022-04-12','2022-04-13','2022-04-14','2022-04-18','2022-04-19','2022-04-20','2022-04-22','2022-04-25','2022-04-26','2022-04-27','2022-04-28','2022-04-29','2022-05-02']
+let label_ano = ['2022-01-03','2022-01-04','2022-01-05','2022-01-06','2022-01-07','2022-01-10','2022-01-11','2022-01-12','2022-01-13','2022-01-14','2022-01-17','2022-01-18','2022-01-19','2022-01-20','2022-01-21','2022-01-24','2022-01-25','2022-01-26','2022-01-27','2022-01-28','2022-01-31','2022-02-01','2022-02-02','2022-02-03','2022-02-04','2022-02-07','2022-02-08','2022-02-09','2022-02-10','2022-02-11','2022-02-14','2022-02-15','2022-02-16','2022-02-17','2022-02-18','2022-02-21','2022-02-22','2022-02-23','2022-02-24','2022-02-25','2022-03-02','2022-03-03','2022-03-04','2022-03-07','2022-03-08','2022-03-09','2022-03-10','2022-03-11','2022-03-14','2022-03-15','2022-03-16','2022-03-17','2022-03-18','2022-03-21','2022-03-22','2022-03-23','2022-03-24','2022-03-25','2022-03-28','2022-03-29','2022-03-30','2022-03-31','2022-04-01','2022-04-04','2022-04-05','2022-04-06','2022-04-07','2022-04-08','2022-04-11','2022-04-12','2022-04-13','2022-04-14','2022-04-18','2022-04-19','2022-04-20','2022-04-22','2022-04-25','2022-04-26','2022-04-27','2022-04-28','2022-04-29','2022-05-02',"2022-05-03", "2022-05-04", "2022-05-05"]
+
+// --------------------------------------Changing visualization----------------------------------------------------------//
+
+var alternate = 0
+function visible_value(){
+    if (alternate == 0) {
+        monkey.innerHTML = "&#128053"
+        bolsa_ibov.innerText = `R$ ${1000}`
+        bolsa_poupanca.innerText = `R$ ${1000}`
+        bolsa_btc.innerText = `R$ ${1000}`
+        bolsa_dollar.innerText = `R$ ${1000}`
+        bolsa_cdi.innerText = `R$ ${1000}`
+        bolsa_selic.innerText = `R$ ${1000}`
+        bolsa_ibov_total.innerText = `R$ ${Math.round(invested_value_ibov * 100) / 100}`
+        bolsa_btc_total.innerText = `R$ ${Math.round(invested_value_btc * 100) / 100}`
+        bolsa_dollar_total.innerText = `R$ ${Math.round(invested_value_dollar * 100) / 100}`
+        bolsa_poupanca_total.innerText = `R$ ${Math.round(invested_value_poupanca * 100) / 100}`
+        bolsa_cdi_total.innerText = `R$ ${Math.round(invested_value_cdi * 100) / 100}`
+        bolsa_selic_total.innerText = `R$ ${Math.round(invested_value_selic * 100) / 100}`
+        total_carteira_valor.innerText = `R$ ${Math.round((invested_value_ibov + invested_value_btc + invested_value_dollar + invested_value_poupanca + invested_value_cdi + invested_value_selic )* 100) / 100}`
+        alternate = 1
+    } else { 
+        monkey.innerHTML = "&#128584"
+        bolsa_ibov.innerText = "******"
+        bolsa_poupanca.innerText = "******"
+        bolsa_btc.innerText = "******"
+        bolsa_dollar.innerText = "******"
+        bolsa_cdi.innerText = "******"
+        bolsa_selic.innerText = "******"
+        bolsa_ibov_total.innerText = `******`
+        bolsa_btc_total.innerText = `******`
+        bolsa_dollar_total.innerText = `******`
+        bolsa_poupanca_total.innerText = `******`
+        bolsa_cdi_total.innerText = `******`
+        bolsa_selic_total.innerText = `******`
+        total_carteira_valor.innerText = `******`
+        alternate = 0 
+    }
+}
 
         
 // ----------------------------------------Scroll no grafico-----------------------------------------------------//
@@ -146,14 +221,13 @@ function checking_for_percentage(dados1, dados2) {
     } return x
 }
 
-percentage_calculator(btc_value, btc_value_percentage)
 
 // -----------------------------------Changing graph----------------------------------------------//
 
 function graph_change() {
-    var ibov = checking_for_percentage(ibovespa_value,ibovespa_value_percentage)
-    var btc = checking_for_percentage(btc_value,btc_value_percentage)
-    var dollar = checking_for_percentage(dollar_value,dollar_value_percentage)
+    var ibov = checking_for_percentage(ibovespa_simulation,ibovespa_value_percentage)
+    var btc = checking_for_percentage(btc_simulation,btc_value_percentage)
+    var dollar = checking_for_percentage(dollar_simulation,dollar_value_percentage)
     if (semanaradio.checked) {
         semanabox.style.background = "#56EEF4"
         mesbox.style.background = "none"
@@ -187,9 +261,9 @@ function graph_change() {
         document.querySelector("#semana>span").style.color = "white"
         mesbox.style.color = "white"
         ano.style.color = "black"
-        graph(ibov, "IBOV", ibovespa_value.length)
-        graph(btc, "BTC", btc_value.length)
-        graph(dollar, "Dollar",dollar_value.length)
+        graph(ibov, "IBOV", ibovespa_simulation.length)
+        graph(btc, "BTC", btc_simulation.length)
+        graph(dollar, "Dollar",dollar_simulation.length)
         arrow_left.style.visibility = "hidden"
         arrow_right.style.visibility = "hidden"
     }    
@@ -212,7 +286,6 @@ function total_viewer() {
         graph_change()
     }
 }
-
 // -----------------------------------------------Reading JSON------------------------------------------------------------//
 
 var poupanca_value = []
@@ -225,7 +298,7 @@ var ibovespa_value = []
 var ibovespa_value_percentage = []
 var selic_value = []
 
-// function read_json (file_name, list){
+// async function read_json (file_name, list){
 //     fetch(file_name)
 //     .then(function(resp){
 //         return resp.json();
@@ -235,8 +308,7 @@ var selic_value = []
 //     }); return list
 // }
 
-// poupanca_value = await read_json('./poupanca.json', poupanca_value)
-// console.log(poupanca_value)
+// poupanca_value = read_json('./json_files/poupanca.json', poupanca_value)
 
 await fetch('./json_files/poupanca.json')
     .then(function(resp){
@@ -303,6 +375,20 @@ await fetch('./json_files/selic.json')
         selic_value = data.map(x => x.valor)
     })
 
+var ibovespa_simulation = []
+var btc_simulation = []
+var dollar_simulation = []
+
+function investment_simulation(final_list, invested,base_graph){
+    for (var i = 0; i < base_graph.length; i++){
+        invested = invested + ((invested * (base_graph[i] * 100)) / 100)
+        final_list.push(invested)
+    } return final_list, invested
+}
+
+ibovespa_simulation, invested_value_ibov= investment_simulation(ibovespa_simulation, invested_value_ibov, ibovespa_value_percentage)
+btc_simulation, invested_value_btc= investment_simulation(btc_simulation, invested_value_btc, btc_value_percentage)
+dollar_simulation, invested_value_dollar= investment_simulation(dollar_simulation, invested_value_dollar, dollar_value_percentage)
 
 // ------------------------------------------Plotting graph-------------------------------------------------------//
 
@@ -310,7 +396,7 @@ const data = {
     labels: label_ano,
     datasets: [{
         label: "IBOV",
-        data: ibovespa_value,
+        data: ibovespa_simulation,
         backgroundColor: [
             '#357DED',   
         ],
@@ -329,28 +415,31 @@ const data = {
         borderColor: [
             'white',
                     ],
+        tension: 0.4,
         borderWidth: 0.8,
     },
     {
         label: 'BTC',
-        data: btc_value,
+        data: btc_simulation,
         backgroundColor: [
                     'orange'
                 ],
         borderColor: [
                     'orange'
                 ],
+        tension: 0.4,
         borderWidth: 0.8,
     },
     {
         label: 'Dollar',
-        data: dollar_value,
+        data: dollar_simulation,
         backgroundColor: [
             'red',
         ],
         borderColor: [
             'red',
                     ],
+        tension: 0.4,
         borderWidth: 0.8,
     },
     {
@@ -362,6 +451,7 @@ const data = {
         borderColor: [
             'yellow',
                     ],
+        tension: 0.4,
         borderWidth: 0.8,
     },
     {
@@ -373,6 +463,7 @@ const data = {
         borderColor: [
             'green',
                     ],
+        tension: 0.4,
         borderWidth: 0.8,
     },]
 };
@@ -386,6 +477,9 @@ const config = {
             y: {
                 grid: {
                     color: "#56EEF4"
+                },
+                ticks: {
+                    color: "white"
                 }
             },
             x: {
@@ -393,7 +487,11 @@ const config = {
                 max: label_ano.length -1, 
                 grid: {
                     color: "#56EEF4"
-                }}
+                },
+                ticks: {
+                    color: "white"
+                }
+            }
             },
             plugins:{      // Configuração de legendas
                 legend:{
@@ -419,10 +517,6 @@ const myChart = new Chart(
     config
     );
     
-            
-var t33 = document.getElementById("fodendo_teste")
-t33.innerHTML = btc_value_percentage
-
             
 
 
--- teste.js
+++ teste.js
@@ -1,13 +1,26 @@
-const poupanca = require('./poupanca.json')
+// const poupanca = require('./poupanca.json')
 
-let poupanca_value = []
+// let poupanca_value = []
 
-function add_value_to_list(json, list){
-    list.push(json.map((data) => data.valor))
+// function add_value_to_list(json, list){
+//     list.push(json.map((data) => data.valor))
 
-    return list
+//     return list
+// }
+
+// poupanca_value = add_value_to_list(poupanca, poupanca_value)
+
+// console.log(`ta aqui ó ................ ${poupanca_value}`)
+
+
+var valor = 200
+
+lista = [0.02,0.04,0.05,0.02,0.06,0.01]
+
+for (var i = 0; i < lista.length; i++){
+    valor = valor + ((valor * (lista[i] * 100)) / 100)
+    console.log(valor)
 }
 
-poupanca_value = add_value_to_list(poupanca, poupanca_value)
 
-console.log(`ta aqui ó ................ ${poupanca_value}`)
+console.log(valor)
